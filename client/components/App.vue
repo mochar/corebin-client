0d143fa51e3bd22154f85bff03cb4a63f64ae4db
Mo Ch <mocharrout@gmail.com>
2016-10-19 09:34:50 +0200

    Added bootstrap and font-awesome as working deps

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app">
+    <div class="container" id="app">
         <p>
             <router-link to="/">Home</router-link>
             <router-link to="/overview">Overview</router-link>
@@ -7,9 +7,25 @@
             <router-link to="/refine">Refine</router-link>
             <router-link to="/help">Help</router-link>
         </p>
+        <button type="button" class="btn btn-outline-primary">
+            <span class="fa fa-cog fa-spin"></span>
+            Primary
+        </button>
         <router-view></router-view>
     </div>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            show: true
+        }
+    }
+}
+</script>
+
 <style>
+@import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+@import "../../node_modules/font-awesome/css/font-awesome.min.css";
 </style>
