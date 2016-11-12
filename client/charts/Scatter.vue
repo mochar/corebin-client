1b13f15fa4edc7b4bb22c84d217c6a56d321d357
Mo Ch <mocharrout@gmail.com>
2016-11-12 20:33:54 +0100

    Added skeleton for scatterplot

---
+++ client/charts/Scatter.vue
@@ -0,0 +1,97 @@
+<template>
+<div></div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+    data() {
+        return {
+            svg: null,
+            view: null,
+            zoom: null,
+            x: null,
+            y: null,
+            xAxis: null,
+            yAxis: null
+        }
+    },
+    
+    props: ['contigs', 'xData', 'yData'],
+    
+    methods: {
+        updatePlot() {
+        },
+        zoomed() {
+            this.view.attr('transform', d3.event.transform)
+            d3.select(this.$el).select('g.x').call(this.xAxis.scale(d3.event.transform.rescaleX(this.x)))
+            d3.select(this.$el).select('g.y').call(this.yAxis.scale(d3.event.transform.rescaleY(this.y)))
+        }
+    },
+
+    mounted() {
+        const width = parseInt(d3.select(this.$el).style('width'), 10)
+        const height = width
+        this.svg = d3.select(this.$el).append('svg').attr('width', width).attr('height', height)
+        
+        this.zoom = d3.zoom()
+            .scaleExtent([1, 40])
+            .translateExtent([[-100, -100], [width + 90, height + 100]])
+            .on('zoom', this.zoomed);
+        
+        this.x = d3.scaleLinear()
+            .domain([d3.min(this.contigs, c => c[xData]), d3.max(this.contigs, c => c[xData])])
+            .range([0, width])
+            
+        this.y = d3.scaleLinear()
+            .domain([d3.min(this.contigs, c => c[yData]), d3.max(this.contigs, c=> c[yData])])
+            .range([0, height])
+        
+        this.xAxis = d3.axisBottom(this.x)
+            .ticks((width + 2) / (height + 2) * 10)
+            .tickSize(height)
+            .tickPadding(8 - height)
+
+        this.yAxis = d3.axisRight(this.y)
+            .ticks(10)
+            .tickSize(width)
+            .tickPadding(8 - width)
+            
+        this.view = this.svg.append('rect')
+            .attr('class', 'view')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('width', width)
+            .attr('height', height)
+            
+        this.svg.append('g')
+            .attr('class', 'x axis hist-axis')
+            .call(this.xAxis)
+        
+        this.svg.append('g')
+            .attr('class', 'y axis hist-axis')
+            .call(this.yAxis)
+            
+        this.svg.call(this.zoom)
+    
+        this.updatePlot()
+    },
+
+    updated: function() {
+        console.log('update')
+        this.updatePlot()
+    }
+}
+</script>
+
+<style>
+.axis path {
+  display: none;
+}
+
+.axis line {
+  stroke-opacity: 0.3;
+  shape-rendering: crispEdges;
+}
+</style>
--- client/views/Refine.vue
+++ client/views/Refine.vue
@@ -1,13 +1,92 @@
 <template>
-    <div>
-        <p>Top refine</p>
+<div class="row">
+    <div class="col-xs-4">
+        <div class="card">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs float-xs-left">
+                    <li class="nav-item">
+                        <a class="nav-link" :class="{active: tab==='bins'}" href="#"
+                            @click.prevent="tab = 'bins'">Bins</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" :class="{active: tab==='selection'}" href="#"
+                            @click.prevent="tab = 'selection'">Selection</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-block">
+                Sup
+            </div>
+            <ul class="list-group list-group-flush">
+                <li v-for="bin in bins" class="list-group-item" :style="{ 'border-left-color': bin.color }">
+                    {{ bin.name }}
+                    <div class="float-xs-right">
+                        <a href="#">view</a> |
+                        <a href="#">plot</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
+    <div class="col-xs-8">
+        <div class="card">
+            <scatter
+                :contigs="contigs"
+                :xData="xData"
+                :yData="yData"
+            ></scatter
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+import Scatter from '../charts/Scatter'
+
 export default {
+    data() {
+        return {
+            tab: 'bins',
+            contigs: [],
+            xData: 'gc',
+            yData: 'length'
+        }
+    },
+    
+    components: {
+        Scatter
+    },
+
+    computed: {
+        bins() {
+            return this.$store.state.bins
+        }
+    }
 }
 </script>
 
 <style>
+.card-header-tabs {
+    margin-bottom: -.5rem;
+}
+</style>
+
+<style scoped>
+.card-header {
+    background-color: white;
+}
+
+.nav li > a {
+    color: #555;
+}
+
+.nav li a:hover, .nav li.dropdown a:focus {
+    background-color: white;
+}
+
+.list-group-item {
+    border-left-width: 3px;
+    border-left-style: solid;
+    padding: .5rem 1rem;
+}
 </style>
