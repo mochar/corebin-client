3c8d053928a429d2ad2b1aea70ae83885f5a498b
Mo Ch <mocharrout@gmail.com>
2016-11-19 13:20:33 +0100

    Empty message when no assemblies are returned

--- client/store/index.js
+++ client/store/index.js
@@ -88,8 +88,11 @@ const actions = {
         ).then((respJobs, respAssemblies) => {
             commit('SET_JOBS', respJobs[0].jobs)
             commit('SET_ASSEMBLIES', respAssemblies[0].assemblies)
-            if (!respAssemblies[0].assemblies.length) return
-            dispatch('SELECT_ASSEMBLY', respAssemblies[0].assemblies[0])
+            if (respAssemblies[0].assemblies.length) {
+                dispatch('SELECT_ASSEMBLY', respAssemblies[0].assemblies[0])
+            } else {
+                commit('SET_MESSAGE', '')
+            }
         })
     },
     GET_BIN_SETS({ commit, state }) {
