2c04ac87911574056fb808f4eaae4a3f9eb17732
Mo Ch <mocharrout@gmail.com>
2017-02-02 18:33:43 +0100

    Added assembly-saving progress messages

--- client/components/Job.vue
+++ client/components/Job.vue
@@ -4,12 +4,19 @@
         <span class="name">{{ job.meta.name }}</span>
     </div>
 
-    <div class="card-block" style="padding: .15rem 1rem 1rem 1rem;">
+    <div class="card-block" style="padding: .15rem 1rem;">
         <div class="progress">
             <div class="progress-bar progress-bar-striped progress-bar-animated"
                  style="width: 100%">
             </div>
         </div>
+
+        <p class="card-text" style="margin-top: .5rem">
+            <small class="text-muted">
+                <span class="fa fa-refresh fa-spin"></span>
+                {{ job.meta.status }}
+            </small>
+        </p>
     </div>
 </div>
 </template>
