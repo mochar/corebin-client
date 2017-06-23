af4852f073193485d48708f9310e614990a37eed
Mo Ch <mocharrout@gmail.com>
2017-06-23 04:21:23 +0200

    Updated webpack build config

--- build/config.js
+++ build/config.js
@@ -4,23 +4,5 @@ const pkg = require('../package')
 module.exports = {
   port: 4000,
   title: 'CoReBIN - Compare and Refine metagenomic bins',
-  vendor: Object.keys(pkg.dependencies),
-  babel: {
-    babelrc: false,
-    presets: [
-      ['es2015', {modules: false}],
-      'stage-1'
-    ],
-    plugins: [
-      'transform-vue-jsx'
-    ]
-  },
-  postcss: [
-    require('autoprefixer')({
-      // Vue does not support ie 8 and below
-      browsers: ['last 2 versions', 'ie > 8']
-    }),
-    require('postcss-nested')
-  ],
-  cssModules: true,
+  publicPath: '/'
 }
