542b2e64aaca914c212052d718367b5b4cf143c2
Mo Char <mocharrout@gmail.com>
2017-11-29 22:25:45 +0100

    Add svg download and other helpers as Vue plugin

---
+++ client/plugins/Helpers.js
@@ -0,0 +1,25 @@
+import svgDownload from './SvgDownload'
+
+let helpers = {
+    mode: function(arr) {
+        // Return the most frequent item in an array.
+        return arr.sort((a,b) =>
+            arr.filter(v => v === a).length - arr.filter(v => v === b).length
+        ).pop()
+    },
+    range: function(start, end, step) {
+        const _end = end || start
+        const _start = end ? start : 0
+        const _step = step || 1
+        return Array((_end - _start) / _step).fill(0).map((v, i) => _start + (i * _step))
+    },
+    downloadSvg: function(svgEl, filename) {
+        svgDownload(svgEl, filename)
+    }
+}
+
+export default {
+    install: (Vue, options) => {
+        Vue.prototype.$helpers = helpers
+    }
+}
